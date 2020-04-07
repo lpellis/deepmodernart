6fe3a0be7a2824ed657a0f0d9a95968d3adab4e6
Loftie <lpellis@gmail.com>
2020-04-07 21:46:31 +0200

    Use large image

--- src/templates/ArticleEntry.vue
+++ src/templates/ArticleEntry.vue
@@ -160,7 +160,7 @@ export default {
       meta: [
         {
           name: 'twitter:card',
-          content: 'summary'
+          content: 'summary_large_image'
         },
         {
           name: 'twitter:title',
