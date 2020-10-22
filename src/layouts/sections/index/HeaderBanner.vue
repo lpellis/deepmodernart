d1b67f3be24639f5b932d0c3506d933f08058471
Loftie <lpellis@gmail.com>
2020-10-22 21:29:20 +0200

    Change the sub heading

--- src/layouts/sections/index/HeaderBanner.vue
+++ src/layouts/sections/index/HeaderBanner.vue
@@ -4,7 +4,7 @@
       <b-row class="justify-content-center text-center">
         <b-col sm="6" lg="6" class="pt-3 pb-3" style="margin-top:60px">
           <p class="display-4 mb-0">Deep Dream Abstract Art.</p>
-          <p class="lead" style="color:gray">An AI dreams about Abstract Art</p>
+          <p class="lead" style="color:gray">A neural net dreams about abstract art</p>
           <g-link class="card-link" to="/articles"><h5>Explore</h5></g-link>
         </b-col>
         <b-col sm="6" lg="6"  class="pt-3 pb-3" >
